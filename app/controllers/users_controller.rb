d1bc310f5df89d18907a710bccf60e78cbdaf42f
bhagathreddy123 <katta_reddy04@yahoo.com>
2024-01-01 03:17:06 -0800

    Edit and Update resource with turbo stream requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,9 +40,11 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("user_row_#{@user.id}", partial: 'users/user', locals: {user: @user}) }
         format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
       else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('remote_modal', partial: 'users/form_modal', locals: {user: @user}) }
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
