8b10db4f2da0640e8af1899564f0002f61d9faa1
bhagathreddy123 <katta_reddy04@yahoo.com>
2024-01-02 01:37:18 -0800

    active record callbacks specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   validates :name, :city, :country,  presence: true
   validates :email, :contact_number, presence: true, uniqueness: true
+  before_save :check_state_present
+
+  def check_state_present
+    self.state = self.country if state.nil?
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,15 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'when creating a user' do
     let(:user) { build :user }
+    let(:user1) {create(:user, state: nil)}
+
     it "should be valid user with all the attributes" do
       expect(user.valid?).to eq(true)
+    end
+
+    
+    it "should be save a state name as country if country doesn't have state" do
+      expect(user1.state).to eq(user1.country)
     end
   end
 end
