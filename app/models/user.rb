484e5c38c97d98c859306b4719c5c790d85adaa8
bhagathreddy123 <katta_reddy04@yahoo.com>
2024-01-02 02:49:54 -0800

    Writing Specs for custom Active Record Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,21 @@ class User < ApplicationRecord
   validates :name, :city, :country,  presence: true
   validates :email, :contact_number, presence: true, uniqueness: true
   before_save :check_state_present
+  validate :validate_country
 
   def check_state_present
     self.state = self.country if state.nil?
   end
+
+  def validate_country
+    return if country.nil?
+    unless User.country_code_list.include?(country)
+      errors.add(:country, 'is not valid')
+    end
+  end
+
+  def self.country_code_list
+    ISO3166::Country.all.map { |country| country.alpha2 }
+  end
+
 end
