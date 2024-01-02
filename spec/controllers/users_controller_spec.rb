9e1dfdb91e2f21855646ab9c375eb5fc0c518c97
bhagathreddy123 <katta_reddy04@yahoo.com>
2024-01-02 08:57:51 -0800

    used before hooks to refactor example blocks

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,11 +4,19 @@ RSpec.describe UsersController do
   describe 'GET index' do
     let(:user) { create :user }
 
-    it 'assigns @users' do 
+    before(:each) do 
       sign_in(user)
       get :index
+    end
+
+    it 'assigns @users' do 
       expect(assigns(:users)).to eq([user])
+      expect(response).to render_template('index')
     end 
+
+    it 'renders the index template' do 
+      expect(response).to render_template('index')
+    end
 
     it 'returns the status code ok' do 
       expect(response).to have_http_status(:ok)	
