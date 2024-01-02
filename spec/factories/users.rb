f74beae7ff08a1c837b197418c526b6def18aee1
bhagathreddy123 <katta_reddy04@yahoo.com>
2024-01-02 07:05:14 -0800

    update user specs after adding devise gem

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.name_with_middle }
     email { Faker::Internet.email }
+    password { "Password@123"}
     contact_number { Faker::PhoneNumber.cell_phone_with_country_code }
     country { User.country_code_list.sample }
     state { 'AP' }
