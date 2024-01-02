f26f6fca3feb2fdc3c077522cfe7968a5f19d757
bhagathreddy123 <katta_reddy04@yahoo.com>
2024-01-02 03:25:34 -0800

    writing specs for ActiveRecord exceptions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   context 'when creating a user' do
     let(:user) { build :user }
     let(:user1) {create(:user, state: nil)}
+    let(:user2) {build(:user, email: user.email, contact_number: user.contact_number)}
 
     it "should be valid user with all the attributes" do
       expect(user.valid?).to eq(true)
@@ -12,6 +13,12 @@ RSpec.describe User, type: :model do
     
     it "should be save a state name as country if country doesn't have state" do
       expect(user1.state).to eq(user1.country)
+    end
+
+    it "should raise invalid record exception for dulplicate email and contact number" do 
+      user.save 
+      expect(user2.save).to eq(false)
+      expect{user2.save!}.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
 end
